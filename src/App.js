41c5a50e1486ea39a546bb4603b7688c07784af2
Krishna47ak <anandakrishna720@gmail.com>
2024-08-29 08:28:14 +0530

    [ configured navigation routes & updates ]

--- src/App.js
+++ src/App.js
@@ -1,25 +1,35 @@
-import logo from './logo.svg';
-import './App.css';
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import { fetchCourses } from "./store/actions/courses";
+import { useEffect } from "react";
+import store from "./store/store";
+import Home from "./screens/Home";
+import CourseDetail from "./screens/CourseDetail";
+import Dashboard from "./screens/Dashboard";
 
-function App() {
+
+const appRouter = createBrowserRouter([
+  {
+    path: "/",
+    element: <Home />
+  },
+  {
+    path: "/:id",
+    element: <CourseDetail />
+  },
+  {
+    path: "/dashboard",
+    element: <Dashboard />
+  },
+])
+
+const App = () => {
+  useEffect(() => {
+    store.dispatch(fetchCourses())
+  }, [])
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    <RouterProvider router={appRouter} />
+  )
 }
 
 export default App;
--- src/api/alemenoApi.js
+++ src/api/alemenoApi.js
@@ -1,7 +1,7 @@
 import axios from 'axios'
 
 const instance = axios.create({
-    baseURL: 'https://run.mocky.io/v3/70c7b6e2-643b-4093-9403-503e33c32a65'
+    baseURL: process.env.REACT_APP_API_KEY
 })
 
 export default instance;
